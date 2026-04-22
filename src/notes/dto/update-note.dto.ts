3bddbbe6a7c41003f4610379479fc02259a10664
TheDrifter408 <nurullahan408@gmail.com>
2026-04-22 23:41:32 +0600

    Added Swagger modules

--- src/notes/dto/update-note.dto.ts
+++ src/notes/dto/update-note.dto.ts
@@ -1,9 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { BaseNoteDto } from './base-note.dto';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class UpdateNoteDto extends PartialType(BaseNoteDto) {
   @IsUUID()
   @IsNotEmpty()
-  id: string;
+  id!: string;
+
+  @IsString()
+  title!: string;
+
+  @IsString()
+  content!: string;
 }
